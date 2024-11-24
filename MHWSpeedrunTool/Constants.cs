9c2c5a2cc00a46418f93b910d07033075716dbad
Joey Murphy <creekside.jm@gmail.com>
2024-11-24 12:58:45 -0800

    Service class and constants for managing track files

---
+++ MHWSpeedrunTool/Constants.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MHWSpeedrunTool
+{
+    internal class Constants
+    {
+        public static string PINK_RATHIAN_ID = "em001_01";
+        public static string KIRIN_ID = "em011_00";
+        public static string KUSHALA_DAORA_ID = "em024_00";
+        public static string TEOSTRA_ID = "em027_00";
+        public static string NERGIGANTE_ID = "em103_00";
+        public static string VAAL_HAZAK_ID = "em115_00";
+        public static string VELKHANA_ID = "em124_00";
+        public static string BLACKVEIL_ID = "em115_05";
+        public static string NAMIELLE_ID = "em125_00";
+
+        public static string FOREST_ID = "st101";
+        public static string DESERT_ID = "st102";
+        public static string CORAL_ID = "st103";
+        public static string VALE_ID = "st104";
+        public static string RECESS_ID = "st105";
+        public static string HOARFROST_ID = "st108";
+
+        static string _appDataPath = "";
+        public static string APP_DATA_PATH
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_appDataPath))
+                {
+                    _appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\MHWSpeedrunTool";
+
+                    if(!Directory.Exists(_appDataPath))
+                    {
+                        Directory.CreateDirectory(_appDataPath);
+                    }
+                }
+
+                return _appDataPath;
+            }
+        }
+
+        public static string? MHW_INSTALL_PATH { get; private set; }
+
+        public static Dictionary<string, List<string>> monsterIdToStageListMap = new Dictionary<string, List<string>>{
+            { 
+                PINK_RATHIAN_ID, 
+                new List<string>{
+                    FOREST_ID,
+                    DESERT_ID,
+                    CORAL_ID,
+                    VALE_ID
+                }
+            },
+            {
+                KIRIN_ID,
+                new List<string>
+                {
+                    CORAL_ID
+                }
+            },
+            {
+                KUSHALA_DAORA_ID,
+                new List<string>
+                {
+                    FOREST_ID
+                }
+            },
+            {
+                TEOSTRA_ID,
+                new List<string>
+                {
+                    DESERT_ID
+                }
+            },
+            {
+                VAAL_HAZAK_ID,
+                new List<string>
+                {
+                    VALE_ID
+                }
+            },
+            {
+                NERGIGANTE_ID,
+                new List<string>
+                {
+                    RECESS_ID
+                }
+            },
+            {
+                VELKHANA_ID,
+                new List<string>{
+                    FOREST_ID,
+                    DESERT_ID,
+                    CORAL_ID,
+                    VALE_ID,
+                    RECESS_ID,
+                    HOARFROST_ID
+                }
+            },
+            {
+                BLACKVEIL_ID,
+                new List<string>{
+                    FOREST_ID,
+                    DESERT_ID,
+                    CORAL_ID,
+                    VALE_ID,
+                    RECESS_ID,
+                    HOARFROST_ID
+                }
+            },
+            {
+                NAMIELLE_ID,
+                new List<string>{
+                    FOREST_ID,
+                    DESERT_ID,
+                    CORAL_ID,
+                    VALE_ID,
+                    RECESS_ID,
+                    HOARFROST_ID
+                }
+            },
+        };
+
+        /**
+         * Loads the constant strings from JSON file, and saves the JSON to _rawJson for quick access later
+         */
+        public static void LoadConstantsFromJson()
+        {
+            // TODO: Properly initialize install path
+            MHW_INSTALL_PATH = @"D:\SteamLibrary\steamapps\common\Monster Hunter World";
+        }
+    }
+}
---
+++ MHWSpeedrunTool/TrackManagement/TrackService.cs
@@ -0,0 +1,88 @@
+﻿namespace MHWSpeedrunTool.TrackManagement
+{
+    internal class TrackService
+    {
+        /**
+         * @param Dictionary<string, int> trackPatterns
+         * Take in a map of monster id to pattern number, and build nativePC based on provided values
+         */
+        public static void BuildNativePc(Dictionary<string, int> trackPatterns)
+        {
+            string nativePcPath = @$"{Constants.APP_DATA_PATH}\nativePC";
+            if (Directory.Exists(@$"{nativePcPath}\stage"))
+            {
+                Directory.Delete(@$"{nativePcPath}\stage", true);
+                Directory.CreateDirectory(@$"{nativePcPath}\stage");
+            }
+
+            foreach(string monsterId in trackPatterns.Keys) {
+                foreach(string stageId in Constants.monsterIdToStageListMap[monsterId])
+                {
+                    LoadPattern(stageId, monsterId, trackPatterns[monsterId]);
+                }
+            }
+        }
+
+        public static void LoadNativePc()
+        {
+            if(Directory.Exists(@$"{Constants.MHW_INSTALL_PATH}\nativePC\stage"))
+            {
+                Directory.Delete(@$"{Constants.MHW_INSTALL_PATH}\nativePC\stage", true);
+            }
+
+            CopyDirectory(@$"{Constants.APP_DATA_PATH}\nativePC", @$"{Constants.MHW_INSTALL_PATH}\nativePC", true);
+        }
+
+        static void LoadPattern(string stageId, string monsterId, int patternNumber)
+        {
+            if (!Directory.Exists(@$"{Constants.APP_DATA_PATH}\nativePC\stage\{stageId}"))
+            {
+                Directory.CreateDirectory(@$"{Constants.APP_DATA_PATH}\nativePC\stage\{stageId}\common\set");
+            }
+
+            for (int i = 1; i <= 3; i++)
+            {
+                if (i != patternNumber)
+                {
+                    File.Copy(
+                        @$"{Constants.APP_DATA_PATH}\TrackFiles\{stageId}_traceSP_{monsterId}_0{patternNumber}.sobj",
+                        @$"{Constants.APP_DATA_PATH}\nativePC\stage\{stageId}\common\set\{stageId}_traceSP_{monsterId}_0{i}.sobj"
+                    );
+                }
+            }
+        }
+
+        static void CopyDirectory(string sourceDir, string destinationDir, bool recursive)
+        {
+            // Get information about the source directory
+            var dir = new DirectoryInfo(sourceDir);
+
+            // Check if the source directory exists
+            if (!dir.Exists)
+                throw new DirectoryNotFoundException(@$"Source directory not found: {dir.FullName}");
+
+            // Cache directories before we start copying
+            DirectoryInfo[] dirs = dir.GetDirectories();
+
+            // Create the destination directory
+            Directory.CreateDirectory(destinationDir);
+
+            // Get the files in the source directory and copy to the destination directory
+            foreach (FileInfo file in dir.GetFiles())
+            {
+                string targetFilePath = Path.Combine(destinationDir, file.Name);
+                file.CopyTo(targetFilePath);
+            }
+
+            // If recursive and copying subdirectories, recursively call this method
+            if (recursive)
+            {
+                foreach (DirectoryInfo subDir in dirs)
+                {
+                    string newDestinationDir = Path.Combine(destinationDir, subDir.Name);
+                    CopyDirectory(subDir.FullName, newDestinationDir, true);
+                }
+            }
+        }
+    }
+}
