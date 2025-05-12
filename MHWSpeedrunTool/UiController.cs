d5a131abe4d6d42f6edc6b5bd40f801cd5e1570b
Joey Murphy <creekside.jm@gmail.com>
2025-05-11 23:13:39 -0700

    4 - Add method for transferring data. Doesn't interact with program settings yet.

--- MHWSpeedrunTool/UiController.cs
+++ MHWSpeedrunTool/UiController.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using static MHWSpeedrunTool.Form1;
 using MHWSpeedrunTool.SteamManagement;
+using System.Net.NetworkInformation;
+using MHWSpeedrunTool.SaveManagement;
 
 namespace MHWSpeedrunTool
 {
@@ -56,6 +58,55 @@ namespace MHWSpeedrunTool
             if(pathLabel != null)
             {
                 pathLabel.Text = GetWorldPathLabelText();
+            }
+        }
+
+        /**
+         * Handles all dialog and calls for transferring save data between the old manager and this tool.
+         * Not currently referenced anywhere, will be triggered from the settings menu once it's created.
+         */
+        public static void TransferDataFromOldSaveManager()
+        {
+            // If any saves exist for world, warn the user that they may overwrite them
+            if(Directory.Exists($@"{Constants.APP_DATA_PATH}\World") && Directory.EnumerateFiles($@"{Constants.APP_DATA_PATH}\World").Any())
+            {
+                DialogResult overwriteResult = MessageBox.Show(
+                    "You have existing save backups for Monster Hunter: World. This operation will overwrite any existing backups that share a name with backups being transferred. Continue?",
+                    "Existing Saves",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Warning
+                );
+                if (overwriteResult == DialogResult.No)
+                {
+                    return;
+                }
+            }
+
+            string oldManagerPath = Interaction.InputBox("Enter the path to the old save manager here. \n (can be found by right clicking it in Task Manager and selecting \"Open file location\").");
+
+            if(oldManagerPath != "")
+            {
+                string outputMessage = "Successfully transferred saves from the old manager!";
+                string outputHeading = "Success!";
+                MessageBoxIcon outputIcon = MessageBoxIcon.None;
+
+                // The TransferSaveListFromOldManager method handles more detailed exceptions and throws a custom exception so the UI can handle user notification
+                try
+                {
+                    SaveDataService.TransferSaveListFromOldManager(oldManagerPath);
+                }
+                catch (SaveDataService.FailedToLoadExistingSavesException)
+                {
+                    outputMessage = "Unable to locate any existing saves in the old save manager.";
+                    outputIcon = MessageBoxIcon.Error;
+                }
+                catch (SaveDataService.FailedToLoadMainSaveException)
+                {
+                    outputMessage = "Unable to locate a main save backup in the old save manager. All non-main backups were successfully transferred.";
+                    outputIcon = MessageBoxIcon.Error;
+                }
+
+                MessageBox.Show(outputMessage, outputHeading, MessageBoxButtons.OK, outputIcon);
             }
         }
 
